b7a087b052f06090797722207f4ed1f24f1ed3d6
Fimil Faneea <fimilfaneea@gmail.com>
2024-01-17 17:30:23 +0530

    Buttons added

--- src/MainPage.js
+++ src/MainPage.js
@@ -9,21 +9,22 @@ const MainPage = () => {
     <div style={{ backgroundColor: '#050505', color: 'white', padding: '10px', width: '100vw', height: '100vh', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
 <img src={logo} alt="Logo" style={{ position: 'absolute', top: '40px', left: '20px',width: '100px' }} />    
     
-    <Link to="./mosq/Homepage">
-      <button style={{ backgroundColor: 'grey', color: 'white', padding: '10px', margin: '10px' }}>Go to Page 1</button>
-    </Link>
-    <Link to="./air/Homepage">
-      <button style={{ backgroundColor: 'grey', color: 'white', padding: '10px', margin: '10px' }}>Go to Page 2</button>
-    </Link>
-    <Link to="./flood/Homepage">
-      <button style={{ backgroundColor: 'grey', color: 'white', padding: '10px', margin: '10px' }}>Go to Page 3</button>
-    </Link>
-    <Link to="./water/Homepage">
-      <button style={{ backgroundColor: 'grey', color: 'white', padding: '10px', margin: '10px' }}>Go to Page 4</button>
-    </Link>
-    <Link to="./noise/Homepage">
-      <button style={{ backgroundColor: 'grey', color: 'white', padding: '10px', margin: '10px' }}>Go to Page 5</button>
-    </Link>
+<Link to="./mosq/Homepage">
+  <button style={{ backgroundColor: 'grey', color: 'white', padding: '10px', width: '100px', height: '50px', margin: '10px', borderRadius: '15px' }}>Mosquito</button>
+</Link>
+<Link to="./air/Homepage">
+  <button style={{ backgroundColor: 'grey', color: 'white', padding: '10px', width: '100px', height: '50px', margin: '10px', borderRadius: '15px' }}>Air</button>
+</Link>
+<Link to="./flood/Homepage">
+  <button style={{ backgroundColor: 'grey', color: 'white', padding: '10px', width: '100px', height: '50px', margin: '10px', borderRadius: '15px' }}>Flood</button>
+</Link>
+<Link to="./water/Homepage">
+  <button style={{ backgroundColor: 'grey', color: 'white', padding: '10px', width: '100px', height: '50px', margin: '10px', borderRadius: '15px' }}>Water</button>
+</Link>
+<Link to="./noise/Homepage">
+  <button style={{ backgroundColor: 'grey', color: 'white', padding: '10px', width: '100px', height: '50px', margin: '10px', borderRadius: '15px' }}>Noise</button>
+</Link>
+
   </div>
   
   );
