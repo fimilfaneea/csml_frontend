07aea462f2eef6fd02557ba7a4dfbb82bc3a273b
Fimil Faneea <fimilfaneea@gmail.com>
2024-01-17 17:26:03 +0530

    Logo Added

--- src/MainPage.js
+++ src/MainPage.js
@@ -3,27 +3,29 @@
 // src/components/MainPage.js
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import logo from './assets/csml_logo.png';
 const MainPage = () => {
   return (
-    <div style={{ backgroundColor: 'black', color: 'white', padding: '20px' }}>
-      <h1>Main Page</h1>
-      <Link to="./mosq/Homepage">
-        <button>Go to Page 1</button>
-      </Link>
-      <Link to="./air/Homepage">
-        <button>Go to Page 2</button>
-      </Link>
-      <Link to="./flood/Homepage">
-        <button>Go to Page 3</button>
-      </Link>
-      <Link to="./water/Homepage">
-        <button>Go to Page 4</button>
-      </Link>
-      <Link to="./noise/Homepage">
-        <button>Go to Page 5</button>
-      </Link>
-    </div>
+    <div style={{ backgroundColor: '#050505', color: 'white', padding: '10px', width: '100vw', height: '100vh', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
+<img src={logo} alt="Logo" style={{ position: 'absolute', top: '40px', left: '20px',width: '100px' }} />    
+    
+    <Link to="./mosq/Homepage">
+      <button style={{ backgroundColor: 'grey', color: 'white', padding: '10px', margin: '10px' }}>Go to Page 1</button>
+    </Link>
+    <Link to="./air/Homepage">
+      <button style={{ backgroundColor: 'grey', color: 'white', padding: '10px', margin: '10px' }}>Go to Page 2</button>
+    </Link>
+    <Link to="./flood/Homepage">
+      <button style={{ backgroundColor: 'grey', color: 'white', padding: '10px', margin: '10px' }}>Go to Page 3</button>
+    </Link>
+    <Link to="./water/Homepage">
+      <button style={{ backgroundColor: 'grey', color: 'white', padding: '10px', margin: '10px' }}>Go to Page 4</button>
+    </Link>
+    <Link to="./noise/Homepage">
+      <button style={{ backgroundColor: 'grey', color: 'white', padding: '10px', margin: '10px' }}>Go to Page 5</button>
+    </Link>
+  </div>
+  
   );
 };
 
