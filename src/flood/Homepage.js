941080a54bfcf2cb6f5b1e4461b361afc5cb08c6
Fimil Faneea <fimilfaneea@gmail.com>
2024-01-17 17:34:21 +0530

    nil

--- src/flood/Homepage.js
+++ src/flood/Homepage.js
@@ -1,14 +1,15 @@
-// src/mosq/homepage.js
-
+// src/mosq/Homepage.js
 import React from 'react';
+import logo from '../assets/csml_logo.png'; // make sure the path to the logo is correct
 
 const Homepage = () => {
   return (
-    <div>
-      <h1>Welcome to the Homepage!</h1>
-      <p>Reserved for mflood.</p>
+    <div style={{ backgroundColor: '#050505', color: 'white', padding: '10px', width: '100vw', height: '100vh', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
+      <img src={logo} alt="Logo" style={{ position: 'absolute', top: '40px', left: '20px', width: '100px' }} />
+      {/* ... rest of your code ... */}
     </div>
   );
 };
 
 export default Homepage;
+  
